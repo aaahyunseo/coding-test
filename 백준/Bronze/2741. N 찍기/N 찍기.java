13598a1a5568a26b3d41b11f1fd69ecf530998c9
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2024-10-29 21:31:50 +0900

    [Bronze V] Title: N 찍기, Time: 748 ms, Memory: 30912 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+
         for(int i=1; i<=n; i++){
             System.out.println(i);
         }
