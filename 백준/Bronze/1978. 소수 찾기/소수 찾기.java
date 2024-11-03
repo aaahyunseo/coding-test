c79c818c133444bf0ecadf2692d43e2752295b6a
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2024-11-03 17:39:57 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -17,9 +17,9 @@ public class Main {
     }
 
     private static int isPrime(int num) {
-        if (num == 1) return 0;
+        if (num < 2) return 0;
         if (num == 2) return 1;
-        for (int i = 2; i < num; i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return 0;
             }
