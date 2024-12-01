59785ddedd0a0eec1365af9a2e5ed6325b38b070
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2024-12-01 18:40:02 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "강한친구 대한육군";
+        for (int i = 0; i < 2; i++) {
+            System.out.println(str);
+        }
+    }
+}
