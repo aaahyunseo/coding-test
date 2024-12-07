4140270ce06bdbbbf2ddc8caf4c4f9f179f81ac4
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2024-12-08 01:04:38 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 112 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n-1; j++) {
+                sb.append(' ');
+            }
+            for (int j = 0; j < i*2+1; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+
+        for (int i = n-1; i > 0; i--) {
+            for (int j = i; j <= n-1; j++) {
+                sb.append(' ');
+            }
+            for (int j = 0; j < i*2-1; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
