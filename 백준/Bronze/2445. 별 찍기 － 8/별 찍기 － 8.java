eec0f113aa91fd8c03116cb02e23edc52c364d4d
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2024-12-09 21:49:13 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 116 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            for (int j = i; j < 2*n-i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = 1; i <= n-1; i++) {
+            for (int j = i; j < n; j++) {
+                sb.append("*");
+            }
+            for (int j = 0; j < 2*i; j++) {
+                sb.append(" ");
+            }
+            for (int j = i; j < n; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
