850a1cae8d3de43595e9030c59b795e3634a933d
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2024-12-12 11:16:25 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Integer.parseInt(st.nextToken());
+            long b = Integer.parseInt(st.nextToken());
+
+            if (a==0 && b==0) break;
+            else if (a>b) System.out.println("Yes");
+            else System.out.println("No");
+        }
+    }
+}
