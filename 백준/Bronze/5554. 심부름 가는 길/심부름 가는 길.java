b9d6128dc3982f9d362ca57de41f2fcdb0c3712e
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2024-12-29 21:16:56 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+
+        int sum = a + b + c + d;
+        sb.append(sum/60).append("\n").append(sum%60);
+        System.out.println(sb);
+    }
+}
