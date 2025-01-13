9c379fa7394ed3deb079003b728825dfdea68667
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2025-01-13 15:54:07 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 564 ms, Memory: 57360 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        int[] prefixSum = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+
+        for (int k = 0; k < m; k++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            sb.append(prefixSum[j] - prefixSum[i-1]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
