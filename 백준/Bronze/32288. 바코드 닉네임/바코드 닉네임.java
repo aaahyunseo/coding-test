fa6de0e5617f5568ec7fbd94cf3d4ed0d6df9edd
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2025-02-07 14:13:00 +0900

    [Bronze IV] Title: 바코드 닉네임, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (c=='I') {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
