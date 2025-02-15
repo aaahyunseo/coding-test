780ce1c0e29907aa9a8ef254314709ed1eb3ce5a
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2025-02-16 00:38:07 +0900

    [Bronze IV] Title: No Brainer, Time: 100 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x >= y) sb.append("MMM BRAINS").append("\n");
+            else sb.append("NO BRAINS").append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
