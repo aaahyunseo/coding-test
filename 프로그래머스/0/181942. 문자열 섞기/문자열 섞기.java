81859dc2ca07d51f79f33219fbabaa138cfc719d
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-03-25 13:32:34 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i=0; i<str1.length(); i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(i);
+            answer.append(c1).append(c2);
+        }
+        
+        return answer.toString();
+    }
+}
