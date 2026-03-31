f6ce0faa1eb08c3955d82106c45c64f8a7806f1a
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-03-31 20:21:21 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean b1 = x1 || x2;
+        boolean b2 = x3 || x4; 
+        
+        return b1 && b2;
+    }
+}
