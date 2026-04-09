54a912e3057d56624e9e32e2a544a69813759a45
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-04-09 19:16:22 +0900

    [level 2] Title: 디펜스 게임, Time: 107.83 ms, Memory: 148 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int round = 0;
+        
+        for(int i = 0; i < enemy.length; i++) {
+            pq.offer(enemy[i]);
+            n -= enemy[i];
+            
+            if(n < 0) {
+                if(k > 0) {
+                    n += pq.poll();
+                    k--;
+                } else break;
+            }
+            
+            round++;
+        }
+        
+        return round;
+    }
+}
