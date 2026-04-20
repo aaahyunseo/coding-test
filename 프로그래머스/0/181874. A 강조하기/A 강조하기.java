5efe8d0c7eea18805c9cc9bfdfa7e72f973aa9fb
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-04-20 15:14:34 +0900

    [level 0] Title: A 강조하기, Time: 15.49 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(char c : myString.toCharArray()) {
+            if(c=='a' || c=='A') answer += "A";
+            else {
+                answer += Character.toLowerCase(c);
+            }
+        }
+        
+        return answer;
+    }
+}
