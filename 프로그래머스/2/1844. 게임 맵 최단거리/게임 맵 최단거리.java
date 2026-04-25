c5be8ec44deaeb8b983d7d570748d0226dc2d982
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-04-25 19:05:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.38 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -7,36 +7,44 @@ class Solution {
     int[] dy = {0, 0, -1, 1};
     int n, m;
     
+    public class Node {
+        int x, y, dist;
+        
+        Node(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+    
     public int solution(int[][] maps) {
         this.maps = maps;
         n = maps.length;
         m = maps[0].length;
-        
         visited = new boolean[n][m];
         
-        return BFS(0, 0);
+        return BFS(0,0);
     }
     
-    public int BFS(int x, int y) {
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{x, y, 1});
-        visited[x][y] = true;
+    public int BFS(int i, int j) {
+        Deque<Node> q = new ArrayDeque<>();
+        q.offer(new Node(i, j, 1));
+        visited[i][j] = true;
         
         while(!q.isEmpty()) {
-            int[] arr = q.poll();
-            int dist = arr[2];
+            Node node = q.poll();
             
-            if(arr[0] == n-1 && arr[1] == m-1 && maps[arr[0]][arr[1]] == 1) return dist;
+            if(node.x == n-1 && node.y == m-1) return node.dist;
             
-            for(int i=0; i<4; i++) {
-                int nx = arr[0] + dx[i];
-                int ny = arr[1] + dy[i];
+            for(int d=0; d<4; d++) {
+                int nx = node.x + dx[d];
+                int ny = node.y + dy[d];
                 
                 if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
-                if(visited[nx][ny] || maps[nx][ny] == 0) continue;
+                if(visited[nx][ny] || maps[nx][ny] != 1) continue;
                 
                 visited[nx][ny] = true;
-                q.offer(new int[]{nx, ny, dist+1});
+                q.offer(new Node(nx, ny, node.dist+1));
             }
         }
         
