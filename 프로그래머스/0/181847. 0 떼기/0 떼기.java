bcdaafb2f62cfc1d30b1a69f0962ff9112c27af2
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-05-09 20:20:54 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String n_str) {
+        int idx = 0;
+        
+        if(n_str.charAt(0) == '0') {
+            for(int i=0; i<n_str.length(); i++) {
+                if(n_str.charAt(i) != '0') {
+                    idx = i;
+                    break;
+                }
+            }
+        }
+        
+        return n_str.substring(idx);
+    }
+}
