f784d8f49168caf3f5bb0189f3b7ce38d07708e0
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-05-13 14:12:16 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b) {
+        int score = 0;
+        
+        if(a%2 != 0 && b%2 != 0) {
+            score = a*a + b*b;
+        } else if(a%2 != 0 || b%2 != 0) {
+            score = 2 * (a + b);
+        } else {
+            score = Math.abs(a-b);
+        }
+        
+        return score;
+    }
+}
