59018a0e315b0752c0ce438d24df906fff8740f4
HYUNSEO <130205490+aaahyunseo@users.noreply.github.com>
2026-05-16 19:54:32 +0900

    [level 0] Title: l로 만들기, Time: 4.42 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : myString.toCharArray()) {
+            if(c < 'l') sb.append('l');
+            else sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
